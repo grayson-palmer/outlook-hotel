79ea55010dd1fb8eb8d23cda1be13d205efdfc59
Grayson Palmer <gpalmer1385@gmail.com>
2020-01-09 18:27:28 -0700

    Add basic and property tests

--- test/Booking-test.js
+++ test/Booking-test.js
@@ -1,8 +1,56 @@
 import chai from 'chai';
 const expect = chai.expect;
 
-describe('See if the tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
-  });
+import Booking from '../src/scripts/Booking.js';
+
+describe('Booking Class', function() {
+  let booking;
+
+  beforeEach(() => {
+    booking = new Booking({
+      id: "5fwrgu4i7k55hl6sz",
+      userID: 9,
+      date: "2020/02/04",
+      roomNumber: 15,
+      roomServiceCharges: [ ]
+    })
+  })
+
+  describe('Basic Tests', function() {
+    it('should be a function', function() {
+      expect(Booking).to.be.a('function');
+    });
+
+    it('should be an instance of the Booking class', function() {
+      expect(booking).to.be.an.instanceOf(Booking);
+    });
+  })
+
+  describe('Property Tests', function() {
+    it('should have an ID', function() {
+      expect(booking.id).to.equal("5fwrgu4i7k55hl6sz");
+    });
+
+    it('should have the ID of the customer who made the booking', function() {
+      expect(booking.userId).to.equal(9);
+    });
+
+    it('should have the date the booking is made for', function() {
+      expect(booking.date).to.equal("2020/02/04");
+    });
+
+    it('should have the room number for the booking', function() {
+      expect(booking.roomNumber).to.equal(15);
+    });
+
+    it('should have a list of the room service charges made', function() {
+      expect(booking.roomServiceCharges).to.deep.equal([]);
+    });
+  })
+
+  describe('Method Tests', function() {
+    it('', function() {
+      expect(booking.).to.equal();
+    })
+  })
 });
