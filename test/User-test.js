4d9b9c4d1715d5cdeeb7f0d019014321bf706a9a
Grayson Palmer <gpalmer1385@gmail.com>
2020-01-13 19:36:36 -0700

    Remove old tests and create new ones

--- test/User-test.js
+++ test/User-test.js
@@ -1,25 +1,14 @@
 import chai from 'chai';
 const expect = chai.expect;
 
-import {User, Customer, Manager} from '../src/scripts/User';
+import User from '../src/scripts/User';
+import userData from '../src/mock-data/users-data.js';
 
 describe('User Class', function() {
   let user;
-  let customer;
-  let manager;
 
   beforeEach(() => {
-    user = new User({
-      id: 1,
-      name: "Leatha Ullrich"
-    });
-    customer = new Customer({
-      id: 1,
-      name: "Leatha Ullrich"
-    });
-    manager = new Manager({
-      id: 0
-    });
+    user = new User(userData[0]);
   });
   
   describe('Basic User Class Tests', function() {
@@ -33,63 +22,22 @@ describe('User Class', function() {
   })
 
   describe('User Property Tests', function() {
-    it('should have a username', function() {
-      expect(user.userName).to.equal('customer1');
+    it('should have an id', function() {
+      expect(user.id).to.equal(1);
     });
 
-    it('should have a password', function() {
-      expect(user.password).to.equal('overlook2019');
-    })
-  })
-
-  describe('Customer Class', function() {
-    describe('Customer Basic Tests', function() {
-      it('should be a function', function() {
-        expect(Customer).to.be.a('function');
-      });
-  
-      it('should be an instance of the Customer class', function() {
-        expect(customer).to.be.an.instanceOf(Customer);
-      });
-
-      it('should be an instance of the User class', function() {
-        expect(customer).to.be.an.instanceOf(User);
-      });
+    it('should have a name', function() {
+      expect(user.name).to.equal('Leatha Ullrich');
     })
 
-    describe('Customer Property Tests', function() {
-      it('should have an id', function() {
-        expect(customer.id).to.equal(1);
+    describe('Amount Spent', function() {
+      it('should start with 0 amount spent', function() {
+        expect(user.amountSpent).to.equal(0);
+      });
+
+      it('should update the amount spent with a total of all previous bookings', function() {
+        user.calculateAmountSpent();
       })
-
-      it('should store the customers name', function() {
-        expect(customer.name).to.equal('Leatha Ullrich')
-      })
-    })
-
-    describe('Customer Method Tests', function() {
-    })
-  })
-
-  describe ('Manager Class', function() {
-    describe('Manager Basic Tests', function() {
-      it('should be a function', function() {
-        expect(Manager).to.be.a('function');
-      });
-  
-      it('should be an instance of the Manager class', function() {
-        expect(manager).to.be.an.instanceOf(Manager);
-      });
-
-      it('should be an instance of the User class', function() {
-        expect(manager).to.be.an.instanceOf(User);
-      })
-    })
-
-    describe('Manager Property Tests', function() {
-    })
-
-    describe('Manager Method Tests', function() {
     })
   })
 });
