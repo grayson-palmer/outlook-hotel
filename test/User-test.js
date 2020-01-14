31a1ad178d28b4e4b2ecad5f816c6d4d3f310cad
Grayson Palmer <gpalmer1385@gmail.com>
2020-01-14 13:39:04 -0700

    Add tests for new property and methods

--- test/User-test.js
+++ test/User-test.js
@@ -3,6 +3,7 @@ const expect = chai.expect;
 
 import User from '../src/scripts/User';
 import userData from '../src/mock-data/users-data.js';
+import bookingData from '../src/mock-data/booking-data.js';
 
 describe('User Class', function() {
   let user;
@@ -34,10 +35,39 @@ describe('User Class', function() {
       it('should start with 0 amount spent', function() {
         expect(user.amountSpent).to.equal(0);
       });
+    })
 
-      it('should update the amount spent with a total of all previous bookings', function() {
-        user.calculateAmountSpent();
+    describe('All Reservations', function() {
+      it('should start with an empty array', function() {
+        expect(user.allReservations).to.deep.equal([]);
+      })
+
+      it('should be able to hold reservations from a method', function() {
+        user.findReservations(bookingData);
+        let userReservations = bookingData.filter(booking => {
+          return booking.userID === user.id;
+        })
+        expect(user.allReservations).to.deep.equal(userReservations);
       })
     })
   })
-});
+  describe('Method Tests', function() {
+    beforeEach(() => {
+      user.findReservations(bookingData);
+    })
+    
+    it('should be able to make a new booking', function() {
+      expect(user.allReservations.length).to.deep.equal(25);
+      user.makeReservation(bookingData[0]);
+      expect(user.allReservations.length).to.deep.equal(26);
+    })
+
+    it('should be able to sort the reservations', function() {
+      //not sure how to test this...
+    })
+
+    // it('should update the amount spent with a total of all previous bookings', function() {
+    //   user.calculateAmountSpent();
+    // })
+  })
+})
