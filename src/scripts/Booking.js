d52eed1815ebc01f6047e2cbc6b7d0ae6e4c5718
Grayson Palmer <gpalmer1385@gmail.com>
2020-01-14 13:36:34 -0700

    Change property name for ease of function

--- src/scripts/Booking.js
+++ src/scripts/Booking.js
@@ -1,13 +1,11 @@
 class Booking {
   constructor(obj) {
     this.id = obj.id;
-    this.userId = obj.userID;
+    this.userID = obj.userID;
     this.date = obj.date;
     this.roomNumber = obj.roomNumber;
     this.roomServiceCharges = obj.roomServiceCharges;
   }
 }
-
-
 
 export default Booking;
