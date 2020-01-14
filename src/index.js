f8ab780814ef40c5ba96f2bc365921938da5d4e3
Grayson Palmer <gpalmer1385@gmail.com>
2020-01-13 19:34:02 -0700

    Add class instantiation and checklogin

--- src/index.js
+++ src/index.js
@@ -8,38 +8,86 @@ import $ from 'jquery';
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 import Hotel from './scripts/Hotel';
+import User from './scripts/User';
+import Booking from './scripts/Booking';
+import Room from './scripts/Room';
+import domUpdates from './scripts/domUpdates';
 
 
 let hotel;
+let currentUser;
+let userIds = [];
+
 $(document).ready(() => {
   hotel = new Hotel();
   hotel.findCurrentDate();
   getGuests();
   getBookings();
   getRooms();
-  console.log(hotel);
-})
+});
+
+$('.submit-button').on('click', checkLogin);
 
 const getGuests = () => {
   fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
     .then(response => response.json())
-    .then(result => hotel.loadGuests(result.users))
+    .then(data => data.users)
+    .then(userData => {
+      userData.forEach(customer => {
+        customer = new User(customer);
+        hotel.guests.push(customer);
+      })
+    })
     .catch(err => console.log('Something wrong with users'));
 }
 
 const getBookings = () => {
   fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
     .then(response => response.json())
-    .then(result => hotel.loadBookings(result.bookings))
+    .then(data => data.bookings)
+    .then(bookingData => {
+      bookingData.forEach(booking => {
+        booking = new Booking(booking);
+        hotel.bookings.push(booking);
+      })
+    })
     .catch(err => console.log('Something wrong with bookings'));
 }
 
 const getRooms = () => {
   fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
     .then(response => response.json())
-    .then(result => hotel.loadRooms(result.rooms))
+    .then(data => data.rooms)
+    .then(roomData => {
+      roomData.forEach(room => {
+        room = new Room(room);
+        hotel.rooms.push(room);
+      })
+    })
     .catch(err => console.log('Something wrong with rooms'));
 }
+
+const getUserIds = () => {
+  hotel.guests.forEach(user => {
+    userIds.push(user.id);
+  })
+}
+
+// Getting an error when formatted as ES6 for some reason...
+function checkLogin() {
+  getUserIds();
+  if ($('#username').val() === 'manager' && $('#password').val() === 'overlook2019') {
+    currentUser = 'manager'
+    domUpdates.showDashboard('manager');
+  } else if (userIds.find(i => `customer${i}` === $('#username').val()) && $('#password').val() === 'overlook2019') {
+    currentUser = hotel.guests.find(g => (`customer${g.id}`) === $('#username').val())
+    currentUser.findReservations(hotel.bookings);
+    domUpdates.showDashboard('customer', currentUser);
+  } else {
+    alert('The username or password entered does not match our system. Please try again.');
+  }
+}
+
 
 
 
